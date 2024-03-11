33ea86cdf83c0ca09ad750a1db5fbe8f1f345f41
Natalia García <ngarcia.bustamante@outlook.com>
2024-03-11 04:14:44 -0300

    Se realiza ejercicio2

---
+++ ejercicio2/assets/js/script2.js
@@ -0,0 +1,32 @@
+//Se seleccionan botones con el ID
+let btn1 = document.getElementById('btn-1')
+let btn2 = document.getElementById('btn-2')
+let btn3 = document.getElementById('btn-3')
+let btn4 = document.getElementById('btn-4')
+let btn5 = document.getElementById('btn-5')
+let btn6 = document.getElementById('btn-6')
+
+//Al hacer click en el botón, la caja sobreescibe su estilo añadiendo el mismo color del botón
+btn1.addEventListener('click', function(){
+    document.getElementById('caja').setAttribute('style', 'border:1px solid black; width: 100%; height: 100%; background-color: #e53e3e');
+})
+
+btn2.addEventListener('click', function(){
+    document.getElementById('caja').setAttribute('style', 'border:1px solid black; width: 100%; height: 100%; background-color: #dd6b20');
+})
+
+btn3.addEventListener('click', function(){
+    document.getElementById('caja').setAttribute('style', 'border:1px solid black; width: 100%; height: 100%; background-color: #faf089');
+})
+
+btn4.addEventListener('click', function(){
+    document.getElementById('caja').setAttribute('style', 'border:1px solid black; width: 100%; height: 100%; background-color: #48bb78');
+})
+
+btn5.addEventListener('click', function(){
+    document.getElementById('caja').setAttribute('style', 'border:1px solid black; width: 100%; height: 100%; background-color: #81e6d9');
+})
+
+btn6.addEventListener('click', function(){
+    document.getElementById('caja').setAttribute('style', 'border:1px solid black; width: 100%; height: 100%; background-color: #d53f8c');
+})
